955394349144516c609d43785b4c3783f823a0ad
Unknown <majewski.piotr@gmail.com>
2017-08-22 11:12:54 +0200

    1

---
+++ Lion/src/main/java/Tests/testClass.java
@@ -0,0 +1,45 @@
+package Tests;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by Piotr Majewski on 2017-08-22.
+ */
+public class testClass {
+
+    @Test
+    public void test1() {
+        System.out.println("test1");
+    }
+
+    @Test
+
+    public void test2() {
+        System.out.println("test2");
+    }
+
+    @Test
+
+    public void test3() {
+        System.out.println("test3");
+    }
+
+    @Test
+
+    public void test4() {
+        System.out.println("test4");
+    }
+
+    @Test
+
+    public void test5() {
+        System.out.println("test5");
+    }
+
+    @Test
+
+    public void test6() {
+        System.out.println("test6");
+    }
+
+}
